9891c09f5241856fd5000ae0739b83133e6b90b7
Romeo <romeocorpuz808@gmail.com>
2018-11-10 11:59:56 -1000

    add sanity check in server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const PORT = process.env.EXPRESS_CONTAINER_PORT || 8080;
 const bp = require('body-parser');
 const routes = require('./routes/api/index');
 
-
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.json('HELLO')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`)
