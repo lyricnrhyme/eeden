27a9c8c14febb600e904c754ff570822b597a57b
Romeo <romeocorpuz808@gmail.com>
2018-11-10 10:32:34 -1000

    built GET method for users by ID route

--- server/routes/api/users/users.js
+++ server/routes/api/users/users.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+
+    const { id } = req.params;
+  
+    Users
+      .where("id", id)
+      .fetch({withRelated: ["purchase_id", "store_id"]})
+      .then(userId => {
+        console.log("\nServer: Display By User ID\n", userId);
+        res.json(userId);
+      })
+      .catch(err => {
+        console.log('err: ', err);
+      })
+  })
+
+
 module.exports = router;
