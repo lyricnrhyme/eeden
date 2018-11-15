904d33c273515a9260bbe0512bc0c7a41dcffa1b
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.home>
2018-11-14 18:48:41 -1000

    Working on sessions

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const PORT = process.env.EXPRESS_CONTAINER_PORT || 8080;
 const routes = require('./routes/api/index');
 const cors = require('cors');
@@ -8,8 +9,25 @@ app.use(cors())
 
 app.use('/api', routes);
 
+app.set('trust proxy', 1)
+app.use(session({
+  secret: 'dreamy cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
 app.get('/', (req, res) => {
-  res.send('<p>Sanity Check</p>')
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end('welcome to the session demo. refresh!')
+  }
+  // res.send('<p>Sanity Check</p>')
 })
 
 app.listen(PORT, () => {
