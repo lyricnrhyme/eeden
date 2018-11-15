da89ed432a3252e5d146122b5ce2c0d507df6cd9
Harsh Kotak <kotak_harsh@hotmail.com>
2018-11-14 18:01:15 -1000

    🚑 temp fix

--- src/components/DreamComponent.js
+++ src/components/DreamComponent.js
@@ -6,6 +6,19 @@ export const DreamList = (props) => {
 
   return props.dreamProps.map(dreams =>
 
+    // <Link to={"/dreams/" + dreams.id}>
+    //   <div key={dreams.id} className="dream-list" onClick={() => props.getDream(dreams.id)}>
+    //     <div className="img-wrapper">
+    //       <img className="dream-images" src={dreams.dream_images} alt="No Images" />
+    //     </div>
+    //     <div className="copy">
+    //       <h3>{dreams.title}</h3>
+    //       <p>${dreams.price}</p>
+    //     </div>
+    //   </div>
+    // </Link>
+
+
     //~~~ this reloads the page and state is updated ~~~//
     <a href={"/dreams/" + dreams.id} key={dreams.id} className="dream-list" onClick={() => props.getDream(dreams.id)}>
       <div className="img-wrapper">
@@ -19,5 +32,6 @@ export const DreamList = (props) => {
     </a>
   )
 }
+
 
 
