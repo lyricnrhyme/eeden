d42a6d72356fdb4b2cfbe7ade72ee7887f35674f
Harsh Kotak <kotak_harsh@hotmail.com>
2018-11-19 21:42:30 -1000

    ⚡️ test

--- src/components/StoreInfoComponent.js
+++ src/components/StoreInfoComponent.js
@@ -3,17 +3,18 @@ import "./styles.css"
 
 
 export const StoreInfo = (props) => {
-    // console.log('STORE COMPONENT PROS: ', props.storeProps);
-    
+  console.log('STORE COMPONENT PROS: ', props.storeProps);
+
 
   return <div className="storeinfo">
-      <div className="storename">
-        <h1>{ props.storeProps.title }</h1>
-      </div>
-      <div className="storedescription">
-        { props.storeProps.description }
-      </div>
+    <div className="storename">
+      <h1>{props.storeProps.title}</h1>
     </div>
+    <div className="storedescription">
+      {props.storeProps.description}
+    </div>
+    {/* <div className="store_owner">Owner: {props.storeProps.created_by}</div> */}
+  </div>
 }
 
 // StoreInfo.defaultProps = {
